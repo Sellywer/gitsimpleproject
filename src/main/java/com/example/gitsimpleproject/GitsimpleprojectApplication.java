37c859f04b274c8cac5eece57cf6b028047404c2
Adel Zakirova <AZakirova@ibs.ru>
2022-03-03 16:51:30 +0500

    init simple project

---
+++ src/main/java/com/example/gitsimpleproject/GitsimpleprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitsimpleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitsimpleprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitsimpleprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitsimpleproject/GitsimpleprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitsimpleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitsimpleprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
